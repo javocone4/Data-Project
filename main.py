35cffa41183b434a17493b00bd83bd145fe1b785
Javier <fcojacopu4@gmail.com>
2022-11-18 18:08:23 +0100

    c

---
+++ main.py
@@ -0,0 +1,17 @@
+import csv
+import requests
+import time
+import pandas as pd
+import os
+
+
+
+
+
+
+
+URL = "hhttps://valencia.opendatasoft.com/explore/dataset/centros-educativos-en-valencia/download/?format=json&timezone=Europe/Berlin&lang=es"
+data = requests.get(url = URL)
+
+  
+  
